4d1bb4c2ce509e4154d3f032073e02f3559f2ad2
CLMaykel <cmaikel221@gmail.com>
2026-06-02 15:09:34 -0500

    Update Docente.py
    Importación de la clase Usuario y creación de la clase Docente con sus respectivos atributos, también aplicamos Setter

--- Docente.py
+++ Docente.py
@@ -1,1 +1,24 @@
+from usuario import Usuario
+class Docente(Usuario):                           #se inicializan los atributos heredados y propios
+    def __init__(self, id_usuario, cedula, nombres, apellidos, correo, contraseña, telefono, titulo_profesional, especialidad):
+        super().__init__(id_usuario, cedula, nombres, apellidos, correo, contraseña, telefono) 
+        self.__titulo_profesional = titulo_profesional
+        self.__especialidad = especialidad
+        self.__notas_registradas = []
+#setter
+    @property
+    def titulo_profesional(self):
+        return self.__titulo_profesional
 
+    @property
+    def especialidad(self):
+        return self.__especialidad
+
+    @titulo_profesional.setter
+    def titulo_profesional(self, valor):
+        self.__titulo_profesional = valor
+
+    @especialidad.setter
+    def especialidad(self, valor):
+        self.__especialidad = valor
+      
