075c3f77d3b5ec86e052f9cf403c6e75aa1cdb2d
SimonDiceXx <bryan000123456@gmail.com>
2026-07-07 17:28:46 -0500

    Add ConexionDB class for database connection

--- servicios/BaseD.py
+++ servicios/BaseD.py
@@ -1,1 +1,10 @@
 import pyodbc
+class ConexionDB:
+    def __init__(self):
+        self.server = "localhost"  
+        self.database = "PROYECTOPOO"
+        
+        self.driver = "{ODBC Driver 17 for SQL Server}"
+        
+        self.conn = None
+        self.cursor = None
