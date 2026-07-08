a6456f7fe841d7edbfb1e1cafd04e82acab1ced0
Jaimeq2007 <jostinquijijesuarez@gmail.com>
2026-07-07 22:30:12 -0500

    refiniendo comentarios
    Added comments to methods for better clarity.

--- modelos/carga_academica.py
+++ modelos/carga_academica.py
@@ -1,6 +1,6 @@
 class CargaAcademica:
 
-    def __init__(self, id_carga, estudiante, periodo, total_asignaturas, total_creditos, estado=True):
+    def __init__(self, id_carga, estudiante, periodo, total_asignaturas, total_creditos, estado=True):   # Inicializa los datos de la carga académica del estudiante.
         self.__id_carga = id_carga
         self.__estudiante = estudiante
         self.__periodo = periodo
@@ -8,7 +8,7 @@ class CargaAcademica:
         # Se almacenan los créditos totales del período académico del estudiante
         self.__total_creditos = total_creditos
         self.__estado = estado
-
+# Métodos Getter (lectura)
     @property
     def id_carga(self):
         return self.__id_carga
@@ -32,9 +32,10 @@ class CargaAcademica:
     @property
     def estado(self):
         return self.__estado
-
+# Métodos Setter (modificación)
     @total_creditos.setter
     def total_creditos(self, valor):
+        #Permite modificar el total de créditos.
         # Validación: los créditos no pueden ser negativos, solo se actualiza si son válidos
         if valor < 0:
             print("Los creditos no pueden ser negativos")
@@ -46,6 +47,10 @@ class CargaAcademica:
         self.__estado = valor
 
     def generar_carga(self):
+         """
+        Muestra un resumen de la carga académica del estudiante,
+        indicando el período, número de asignaturas y créditos.
+        """
         print("Carga academica de " + self.__estudiante.nombres + " " + self.__estudiante.apellidos)
         print("Periodo: " + self.__periodo)
         print(str(self.__total_asignaturas) + " asignaturas " + str(self.__total_creditos) + " creditos")
