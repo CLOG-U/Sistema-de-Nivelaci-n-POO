9365c801924d3ee93a1e1b8044e3e370123dc730
SimonDiceXx <bryan000123456@gmail.com>
2026-07-07 19:05:53 -0500

    Create tables for Facultad and PeriodoAcademico

--- POOPROYECTO.sql
+++ POOPROYECTO.sql
@@ -4,3 +4,11 @@ CREATE CREATE TABLE Facultad(
     id_facultad INT PRIMARY KEY,
     nombre VARCHAR(100) NOT NULL
 );
+
+CREATE TABLE PeriodoAcademico(
+    id_periodo INT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    fecha_inicio DATE NOT NULL,
+    fecha_fin DATE NOT NULL,
+    estado VARCHAR(20) NOT NULL
+);
