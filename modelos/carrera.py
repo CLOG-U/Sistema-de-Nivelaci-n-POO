aec6c6e9875f6973a3a7b5cda9180ff74e0eac45
Jaimeq2007 <jostinquijijesuarez@gmail.com>
2026-07-07 22:18:17 -0500

    refiniendo comentarios

--- modelos/carrera.py
+++ modelos/carrera.py
@@ -1,14 +1,14 @@
-from modelos.facultad import Facultad
+from modelos.facultad import Facultad   # Importa la clase Facultad desde el módulo modelos.facultad
 
 class Carrera:
 
-    def __init__(self, id_carrera, codigo, nombre, estado, facultad):
+    def __init__(self, id_carrera, codigo, nombre, estado, facultad):   # Inicializa los atributos de la carrera.
         self.__id_carrera = id_carrera
         self.__codigo = codigo
         self.__nombre = nombre
         self.__estado = estado
         self.__facultad = facultad
-#getter 
+ # Métodos Getter (lectura)
     @property
     def id_carrera(self):
         return self.__id_carrera
@@ -28,12 +28,16 @@ class Carrera:
     @property
     def facultad(self):
         return self.__facultad
-#setter
+# Método Setter (modificación)
     @nombre.setter
     def nombre(self, valor):
         self.__nombre = valor
 
 #se muestra la informacion de la carrera
+            """
+        Muestra  la información principal de la carrera,
+        incluyendo su nombre, código y la facultad a la que pertenece.
+        """
     def mostrar_info(self):
         print("Carrera: " + self.__nombre + " codigo: " + self.__codigo)
         print("Facultad: " + self.__facultad.nombre)
